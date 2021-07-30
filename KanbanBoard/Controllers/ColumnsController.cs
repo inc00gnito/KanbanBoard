6b6e86d7dc61fcf78979062eb95c79406f6aeb29
inc0 <patrycja.kozak14@gmail.com>
2021-07-30 15:00:45 +0200

    Columns controller with views

---
+++ KanbanBoard/Controllers/ColumnsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KanbanBoard.Data;
+using KanbanBoard.Models;
+
+namespace KanbanBoard.Controllers
+{
+    public class ColumnsController : Controller
+    {
+        private readonly KanbanContext _context;
+
+        public ColumnsController(KanbanContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Columns
+        public async Task<IActionResult> Index()
+        {
+            var kanbanContext = _context.Columns.Include(c => c.Board);
+            return View(await kanbanContext.ToListAsync());
+        }
+
+        // GET: Columns/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var column = await _context.Columns
+                .Include(c => c.Board)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (column == null)
+            {
+                return NotFound();
+            }
+
+            return View(column);
+        }
+
+        // GET: Columns/Create
+        public IActionResult Create()
+        {
+            ViewData["BoardId"] = new SelectList(_context.Boards, "Id", "Name");
+            return View();
+        }
+
+        // POST: Columns/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,BoardId")] Column column)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(column);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BoardId"] = new SelectList(_context.Boards, "Id", "Name", column.BoardId);
+            return View(column);
+        }
+
+        // GET: Columns/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var column = await _context.Columns.FindAsync(id);
+            if (column == null)
+            {
+                return NotFound();
+            }
+            ViewData["BoardId"] = new SelectList(_context.Boards, "Id", "Name", column.BoardId);
+            return View(column);
+        }
+
+        // POST: Columns/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,BoardId")] Column column)
+        {
+            if (id != column.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(column);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ColumnExists(column.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BoardId"] = new SelectList(_context.Boards, "Id", "Name", column.BoardId);
+            return View(column);
+        }
+
+        // GET: Columns/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var column = await _context.Columns
+                .Include(c => c.Board)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (column == null)
+            {
+                return NotFound();
+            }
+
+            return View(column);
+        }
+
+        // POST: Columns/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var column = await _context.Columns.FindAsync(id);
+            _context.Columns.Remove(column);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ColumnExists(int id)
+        {
+            return _context.Columns.Any(e => e.Id == id);
+        }
+    }
+}
